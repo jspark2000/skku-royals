b6f1023115378c89fe13aa974437b43d5d2980eb
jspark2000 <idpjs2000@gmail.com>
2023-01-24 02:10:54 +0900

    feat: add oauth2 url
    band 로그인 페이지로 이동하는 기능을 추가합니다.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Get('login')
   getLoginPage(@Res() res: Response) {
-    return res.render('pages/auth/login');
+    const oauth2URL: string = this.authService.getOAuth2URL();
+    return res.render('pages/auth/login', { oauth2URL });
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  private readonly getAuthCodeURL = 'https://auth.band.us/oauth2/authorize';
+  private readonly getTokenURL = 'https://auth.band.us/oauth2/token';
+  private readonly getUserProfileURL = 'https://openapi.band.us/v2/profile';
+
+  constructor(private readonly prismaService: PrismaService) {}
+
+  getOAuth2URL(): string {
+    return `${this.getAuthCodeURL}?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_url=${process.env.REDIRECT_URL}`;
+  }
+}
