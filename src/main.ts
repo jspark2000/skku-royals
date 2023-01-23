e209cad7a68f404bf6f48f07f5535b81b183fa36
Jisoo Park <102132161+jspark2000@users.noreply.github.com>
2023-01-23 17:25:45 +0900

    feat: add PostgreSQL session store (#12)
    postgresql을 Session 저장소로 사용하기 위한 설정들을 추가합니다.
    
    Close #11

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import { Client } from 'pg';
 import * as session from 'express-session';
 
 async function bootstrap() {
@@ -11,13 +12,32 @@ async function bootstrap() {
   app.set('trust proxy', 1);
   app.enableCors();
 
+  const conObject = {
+    user: process.env.SESSION_USER,
+    host: process.env.SESSION_HOST,
+    database: process.env.SESSION_DATABASE,
+    password: process.env.SESSION_PASSWORD,
+    port: parseInt(process.env.SESSION_PORT, 10),
+  };
+
+  const sessionClient = new Client(conObject);
+  sessionClient.connect();
+
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const sessionStore = new (require('connect-pg-simple')(session))({
+    conObject,
+  });
+
   app.use(
     session({
-      secret: 'my-secret',
+      store: sessionStore,
+      secret: process.env.SESSION_SECRET,
       resave: false,
       saveUninitialized: false,
       cookie: {
+        maxAge: 1000 * 60 * 60 * 24,
         httpOnly: true,
+        sameSite: false,
         // secure: true,
       },
     }),
