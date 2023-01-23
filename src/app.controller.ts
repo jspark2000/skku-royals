00d64452a789e7ac0589b3c10ae199e57942da3f
Jisoo Park <102132161+jspark2000@users.noreply.github.com>
2023-01-23 15:54:36 +0900

    feat: add settings for ejs (#6)
    ejs를 사용하기 위한 설정들을 추가합니다.
    
    Close #5

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async prismaTest() {
-    return await this.appService.prismaTest();
+  @Render('index')
+  async ejsTest() {
+    return { message: 'ejs TEST' };
   }
 }
