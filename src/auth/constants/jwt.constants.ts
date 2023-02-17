995c708fe6dd172be4095f691c1bd748f5b0028b
Jisoo Park <102132161+jspark2000@users.noreply.github.com>
2023-02-17 09:41:07 +0900

    fix: enable https option

--- src/auth/constants/jwt.constants.ts
+++ src/auth/constants/jwt.constants.ts
@@ -4,7 +4,7 @@ export const REFRESH_TOKEN_EXPIRATION_SEC = 86400;
 export const REFRESH_TOKEN_COOKIE_OPTIONS = {
   maxAge: REFRESH_TOKEN_EXPIRATION_SEC * 1000,
   httpOnly: true,
-  // secure: true,
+  secure: true,
 };
 
 export const AUTH_TYPE = 'Bearer';
