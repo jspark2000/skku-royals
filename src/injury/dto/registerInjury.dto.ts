7a95022ce2f61420377d8a8e3f25c8c4497bb300
jspark2000 <idpjs2000@gmail.com>
2023-03-20 05:10:07 +0000

    feat: add injury services

--- src/injury/dto/registerInjury.dto.ts
+++ src/injury/dto/registerInjury.dto.ts
@@ -2,8 +2,12 @@ import { Type } from 'class-transformer'
 import { IsDate, IsNumber, IsString } from 'class-validator'
 
 export class RegisterInjuryDTO {
+  @IsString()
+  name: string
+
   @IsNumber()
-  peopleId: number
+  @Type(() => Number)
+  studentNo: number
 
   @IsDate()
   @Type(() => Date)
