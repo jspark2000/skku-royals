34b02f1581208676761dc5bf9646798ae983cb0f
jspark2000 <idpjs2000@gmail.com>
2023-06-05 07:53:08 +0000

    deploy test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+. /home/ec2-user/.nvm/nvm.sh
+nvm install --lts
+npm install -g pnpm
 pnpm install -g pm2
 
-PM2_PID=$(pgrep -f pm2)
+cd /home/ec2-user
 
-if [ -z $PM2_PID ]
+pnpm migrate:prod
+pnpm build
+
+PM2=$(pgrep -f pm2)
+
+if [ -z $PM2 ]
 then
-  pm2 start /home/ec2-user/action/main.js -i 0 --name royals_api_server
+  pnpm start:prod
 else
-  pm2 reload royals_api_server
+  pnpm start:reload
 fi
