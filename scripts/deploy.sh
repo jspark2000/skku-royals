248f5d1e49790db9712a089cc2261894cf93aaf1
Jisoo Park <102132161+jspark2000@users.noreply.github.com>
2023-06-06 18:43:01 +0900

    feat(deploy): apply codedeploy (#237)
    * feat(deploy): add some deploy methods
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * deploy test
    
    * feat(deploy): add codeploy on cd method
    
    * update aws-actions/configure-aws-credentials to v2

---
+++ scripts/deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+. ~/.nvm/nvm.sh
+nvm install --lts
+npm update -g
+
+cd /home/ec2-user/action
+
+npm install
+npx prisma generate
+npm run migrate:prod
+npm run build
+
+PM2=$(pm2 ls | grep royals)
+LEN=${#PM2}
+
+if [ ${LEN} -lt 10 ]
+then
+  npm run start:prod
+else
+  npm run start:reload
+fi
